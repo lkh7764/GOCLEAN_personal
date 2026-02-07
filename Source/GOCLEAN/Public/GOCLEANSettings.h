03e464b1e360fb6430d242679f1889985ffe33a6
lkh7764 <100886992+lkh7764@users.noreply.github.com>
2026-02-08 03:40:16 +0900

    [FEAT] Implement GOCLEAN setting (Implement)
    - Add properties for pollution
    - Fix property's name
    - Implement PostEditChangeProperty: pollution LV by Max
    - Set default values in editor

--- Source/GOCLEAN/Public/GOCLEANSettings.h
+++ Source/GOCLEAN/Public/GOCLEANSettings.h
@@ -15,11 +15,44 @@ class GOCLEAN_API UGOCLEANSettings : public UDeveloperSettings
 public:
     // UGOCLEANSettings();
 
+
+    // Participant // 
     UPROPERTY(Config, EditAnywhere, Category = "Participant", meta = (ClampMin = "1", ClampMax = "10"))
-    int32 MaxParticipant;
+    int32 ParticipantMax;
 
-    UPROPERTY(Config, EditAnywhere, Category="Equipment", meta=(ClampMin = "1", ClampMax = "10"))
-    int32 MaxEquipmentSlots;
 
+    // Equipment //
+    UPROPERTY(Config, EditAnywhere, Category="Equipment|Slot", meta=(ClampMin = "1", ClampMax = "10"))
+    int32 EquipmentSlotMax;
+
+    UPROPERTY(Config, EditAnywhere, Category="Equipment|Pollution", meta=(ClampMin = "0.0", ClampMax = "100.0"))
+    float PollutionMax;
+
+    UPROPERTY(Config, VisibleAnywhere, Category = "Equipment|Pollution|Level", meta = (ClampMin = "0.0", ClampMax = "100.0"))
+    float PollutionThreshold_LV0;
+
+    UPROPERTY(Config, VisibleAnywhere, Category = "Equipment|Pollution|Level", meta = (ClampMin = "0.0", ClampMax = "100.0"))
+    float PollutionThreshold_LV1;
+
+    UPROPERTY(Config, VisibleAnywhere, Category = "Equipment|Pollution|Level", meta = (ClampMin = "0.0", ClampMax = "100.0"))
+    float PollutionThreshold_LV2;
+
+    UPROPERTY(Config, VisibleAnywhere, Category = "Equipment|Pollution|Level", meta = (ClampMin = "0.0", ClampMax = "100.0"))
+    float PollutionThreshold_LV3;
+
+    // WaterPollution += MopPollution * WaterPollutionByMop
+    UPROPERTY(Config, EditAnywhere, Category = "Equipment|Pollution|Water", meta = (ClampMin = "0.0", ClampMax = "100.0"))
+    float WaterPollutionByMop;
+
+    UPROPERTY(Config, EditAnywhere, Category = "Equipment|Pollution|Water", meta = (ClampMin = "0.0", ClampMax = "100.0"))
+    float WaterPollutionByAutoMop;
+
+
+    // Getter // 
     static const UGOCLEANSettings* Get() { return GetDefault<UGOCLEANSettings>(); }
+
+
+    // Editor //
+    void PostEditChangeProperty(struct FPropertyChangedEvent& PropertyChangedEvent) override;
+
 };
