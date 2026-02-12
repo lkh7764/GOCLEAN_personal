04134a56d6a056aceb01603cadfa02308ce58ab6
lkh7764 <100886992+lkh7764@users.noreply.github.com>
2026-02-12 18:51:12 +0900

    [FEAT] Implement character-object interaction
    - GOCLEANCharacter: Add anim
    - GOCLEANCharacter: Add equip comp
    - GOCLEANCharacter: Add interaction comp

--- Source/GOCLEAN/Private/GCharacter/GOCLEANCharacter.cpp
+++ Source/GOCLEAN/Private/GCharacter/GOCLEANCharacter.cpp
@@ -14,6 +14,11 @@
 #include "GUI/Manager/GUIManager.h"
 #include "GameFramework/CharacterMovementComponent.h"
 
+#include "GPlayerSystem/GEquipment/GEquipmentComponent.h"
+#include "GPlayerSystem/InteractionComponent.h"
+#include "GTypes/IGInteractable.h"
+#include "Net/UnrealNetwork.h"
+
 
 DEFINE_LOG_CATEGORY(LogTemplateCharacter);
 
@@ -59,6 +64,10 @@ AGOCLEANCharacter::AGOCLEANCharacter()
 	bIsRecoveringStamina = false;
 	bIsCrouching = false;
 	bIsSprinting = false;
+
+
+	InteractionComp = CreateDefaultSubobject<UInteractionComponent>(TEXT("InteractionComponent"));
+	EquipComp = CreateDefaultSubobject<UGEquipmentComponent>(TEXT("EquipmentComponent"));
 }
 
 
@@ -117,6 +126,10 @@ void AGOCLEANCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCo
 
 		// Flashlight toggle
 		EnhancedInputComponent->BindAction(FlashlightAction, ETriggerEvent::Started, this, &AGOCLEANCharacter::ToggleFlashlight);
+
+
+		// Interact
+		EnhancedInputComponent->BindAction(InteractAction, ETriggerEvent::Started, this, &AGOCLEANCharacter::DoInteraction);
 	}
 	else
 	{
@@ -249,3 +262,43 @@ void AGOCLEANCharacter::ToggleFlashlight()
 {
 	FlashlightComp->ToggleVisibility();
 }
+
+
+
+// Equipments & Interaction //
+void AGOCLEANCharacter::DoInteraction()
+{
+	if (!InteractionComp || !EquipComp) return;
+
+	// change anim id to active-anim
+	//		empty
+
+	// require rpc function
+	//		empty
+}
+
+void AGOCLEANCharacter::Server_TryInteraction(FName EquipID)
+{
+	AActor* Target = InteractionComp->GetCurrentTarget();
+	if (Target)
+	{
+		IGInteractable* Interactable = Cast<IGInteractable>(Target);
+		if (Interactable)
+		{
+			if (Interactable->CanInteract(EquipID))
+			{
+				Interactable->ExecuteInteraction(EquipID);
+				UE_LOG(LogTemp, Log, TEXT("[GCharacter] Interaction Executed on %s"), *Target->GetName());
+			}
+		}
+	}
+}
+
+void AGOCLEANCharacter::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(AGOCLEANCharacter, EquipComp);
+	DOREPLIFETIME(AGOCLEANCharacter, AnimID);
+}
+
