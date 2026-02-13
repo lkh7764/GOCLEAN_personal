2d37a559aeb2d5392d6ecf0e2e74b2b2da72da97
lkh7764 <100886992+lkh7764@users.noreply.github.com>
2026-02-14 00:22:54 +0900

    [FIX] Fix datatables
    - Equip: separate FP/TP anim id
    - Obj: register mesh assets
    - file migration LKH -> Kyunghwa

--- Source/GOCLEAN/Public/GTypes/DataTableRow/GEquipmentDataRow.h
+++ Source/GOCLEAN/Public/GTypes/DataTableRow/GEquipmentDataRow.h
@@ -24,26 +24,44 @@ public:
 
 
     // Visual //
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Visual|AnimID")
-    int32 IdleAnimID;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Visual|AnimID|First")
+    int32 IdleAnimID_First;
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Visual|AnimID")
-    int32 IdleToActivateAnimID;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Visual|AnimID|First")
+    int32 IdleToActivateAnimID_First;
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Visual|AnimID")
-    int32 ActivateToIdleAnimID;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Visual|AnimID|First")
+    int32 ActivateToIdleAnimID_First;
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Visual|AnimID")
-    int32 ActivateAnimID;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Visual|AnimID|First")
+    int32 ActivateAnimID_First;
 
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Visual|AnimID")
-    int32 SwapAnimID;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Visual|AnimID|First")
+    int32 SwapAnimID_First;
+
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Visual|AnimID|Third")
+    int32 IdleAnimID_Third;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Visual|AnimID|Third")
+    int32 IdleToActivateAnimID_Third;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Visual|AnimID|Third")
+    int32 ActivateToIdleAnimID_Third;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Visual|AnimID|Third")
+    int32 ActivateAnimID_Third;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Visual|AnimID|Third")
+    int32 SwapAnimID_Third;
+
 
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Visual|Mesh")
     TSoftObjectPtr<UStaticMesh> SM_MeshAsset;
 
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Visual|Mesh")
     TSoftObjectPtr<USkeletalMesh> SK_MeshAsset;
+
 
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Visual|Socket")
     FVector RelativeLocation_ManThird;
